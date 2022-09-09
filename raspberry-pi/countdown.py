4ea598f72f45128edc586bc6bd694b74e8a1bb76
Sam Funk <55893857+sfunk02@users.noreply.github.com>
2022-09-09 10:11:30 -0400

    Update countdown.py

--- raspberry-pi/countdown.py
+++ raspberry-pi/countdown.py
@@ -2,7 +2,7 @@
 import board
 import time
 
-for x in range(10):
-  print (10 - x)
+for x in range(10): #counts 10 numbers, so 0 to 9
+  print (10 - x)    #reverses the order so it starts at 10
   time.sleep(1)
 print("Liftoff")

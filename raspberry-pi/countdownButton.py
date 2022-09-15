4b2ebdde7a06290cd5f50d2ae9ff216f7a400730
sfunk02 <sfunk02@charlottesvilleschools.org>
2022-09-15 10:44:45 -0400

    updated CountdownButton.py

--- raspberry-pi/countdownButton.py
+++ raspberry-pi/countdownButton.py
@@ -12,22 +12,37 @@ G = digitalio.DigitalInOut(board.GP28)
 R.direction = digitalio.Direction.OUTPUT    #sets pin to output
 G.direction = digitalio.Direction.OUTPUT
 
-i = 1
+switchstate = False
 
 while True:
-    if button.value and i == 1:    #if button is pressed
-        i = 0
-        for x in range(10): #counts 10 numbers, so 0 to 9
-            if button.value and i == 1: #not doing anything right now
-                print("Abort")
-                i = 1
-            elif i == 0:
-                print (10 - x)    #reverses the order so it starts at 10
-                R.value = True      #turns red LED on
-                time.sleep(.1)
-                R.value = False     #turns red LED off
-                time.sleep(.9)
-        print("Liftoff")
-
-        while True:
-            G.value = True      #turns on green LED forever
+    Abort = False
+    if button.value == True and switchstate == False:    #if button is pressed
+        switchstate = True
+        for x in range(10):     #counts 10 numbers, so 0 to 9
+            print (10 - x)    #reverses the order so it starts at 10
+            if button.value == False and switchstate == True:
+                switchstate = False
+            elif button.value == True and switchstate == False:
+                    print("Abort")
+                    Abort = True
+                    break
+            R.value = True      #turns red LED on
+            time.sleep(.1)
+            if button.value == False and switchstate == True:
+                switchstate = False
+            elif button.value == True and switchstate == False:
+                    print("Abort")
+                    Abort = True
+                    break
+            R.value = False     #turns red LED off
+            time.sleep(.9)
+            if button.value == False and switchstate == True:
+                switchstate = False
+            elif button.value == True and switchstate == False:
+                    print("Abort")
+                    Abort = True
+                    break
+        if Abort == False:
+            print("Liftoff")
+            while True:
+                G.value = True      #turns on green LED forever

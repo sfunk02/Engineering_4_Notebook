a2c6b7a403998900b02e1e46052c0c4ad1bb7f69
sfunk02 <sfunk02@charlottesvilleschools.org>
2022-09-19 10:03:55 -0400

    updated countdownServo.py

--- raspberry-pi/countdownServo.py
+++ raspberry-pi/countdownServo.py
@@ -3,7 +3,7 @@ import board
 import digitalio
 import pwmio
 import time
-from adafruit_motor import servo
+from adafruit_motor import servo    #imports servo library
 
 button = digitalio.DigitalInOut(board.GP27) #identifies button pin
 button.direction = digitalio.Direction.INPUT    #identifies GP27 as input
@@ -14,13 +14,13 @@ G = digitalio.DigitalInOut(board.GP28)
 R.direction = digitalio.Direction.OUTPUT    #sets pin to output
 G.direction = digitalio.Direction.OUTPUT
 
-pwm_servo = pwmio.PWMOut(board.GP0, duty_cycle=2 ** 15, frequency=50)
-servo1 = servo.Servo(pwm_servo, min_pulse=500, max_pulse=2500)
+pwm_servo = pwmio.PWMOut(board.GP0, duty_cycle=2 ** 15, frequency=50)   #defines pwm output for the servo
+servo1 = servo.Servo(pwm_servo, min_pulse=500, max_pulse=2500)      #defines the servo object
 
 switchstate = False     #switchstates make it so a button doesn't spam commands
 switchstate2 = False
 
-servo1.angle = 5
+servo1.angle = 5    #tells the servo where to start
 
 while True:
     Abort = False
@@ -60,6 +60,6 @@ while True:
                     break
         if Abort == False:      #Liftoff only prints if Abort has not been activated
             print("Liftoff")
-            servo1.angle = 175
+            servo1.angle = 175      #turns servo
             while True:
                 G.value = True      #turns on green LED forever

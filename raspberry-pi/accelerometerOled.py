fea4d90db5473fc24c5ca4c5d6a2c8988c3b3716
sfunk02 <sfunk02@charlottesvilleschools.org>
2022-09-22 10:45:14 -0400

    added accelerometerOled.py

---
+++ raspberry-pi/accelerometerOled.py
@@ -0,0 +1,32 @@
+# type: ignore
+import board
+import busio
+import digitalio
+import time
+import adafruit_mpu6050
+
+sda_pin = board.GP16
+scl_pin = board.GP17
+i2c = busio.I2C(scl_pin, sda_pin)
+
+R = digitalio.DigitalInOut(board.GP1)   #identifies pin on the pico that will power the LED
+R.direction = digitalio.Direction.OUTPUT    #sets pin to output
+
+mpu = adafruit_mpu6050.MPU6050(i2c)
+
+initial = time.monotonic()
+
+while True:
+    now = time.monotonic()
+    if now - initial > 1:
+        print(f"X Acceleration: {mpu.acceleration[0]} m/s^2")   #prints x
+        print(f"Y Acceleration: {mpu.acceleration[1]} m/s^2")
+        print(f"Z Acceleration: {mpu.acceleration[2]} m/s^2")
+        print("")       #blank line
+        now = 0
+    if mpu.acceleration[0] > 9.5 or mpu.acceleration[0] < -9.5:   #if board is tilted in x direction
+        R.value = True                                              #led turns on
+    elif mpu.acceleration[1] > 9.5 or mpu.acceleration[1] < -9.5:     #if board is tilted in y direction
+        R.value = True
+    else:
+        R.value = False

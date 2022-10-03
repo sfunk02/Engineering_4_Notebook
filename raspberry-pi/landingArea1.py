dda3ece611df7fc6380b143f8954ee228e0f02e5
sfunk02 <sfunk02@charlottesvilleschools.org>
2022-10-03 10:31:31 -0400

    updated/commented landingArea1.py

--- raspberry-pi/landingArea1.py
+++ raspberry-pi/landingArea1.py
@@ -1,24 +1,28 @@
 # type: ignore
 
-def triangle_area(x1, y1, x2, y2, x3, y3):
-    total = (x1 * (y1 - y2) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2
-    return abs(total)
+def triangle_area(x1, y1, x2, y2, x3, y3):  #defines a function for triangle area that takes six inputs
+    total = (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2 #the math behind the function, using the six inputs
+    return abs(total)   #returns absolute value of previous math
 
-print("Enter the first coord")
-first = input()
-first = first.split(",")
-x1 = float(first[0])
-y1 = float(first [1])
-print("Enter the second coord")
-second = input()
-second = second.split(",")
-x2 = float(second[0])
-y2 = float(second [1])
-print("Enter the third coord")
-third = input()
-third = third.split(",")
-x3 = float(third[0])
-y3 = float(third [1])
+while True:
+    try:        #tries this code, runs normally with no errors
+        print("Enter the first coordinate in format x,y:")
+        first = input()             #takes first input/coordinate
+        first = first.split(",")    #splits input at the comma, separating into x and y
+        x1 = float(first[0])    #calls on first value (x)
+        y1 = float(first [1])   #calls on second value (y)
+        print("Enter the second coordinate in format x,y:")
+        second = input()
+        second = second.split(",")
+        x2 = float(second[0])
+        y2 = float(second [1])
+        print("Enter the third coordinate in format x,y:")
+        third = input()
+        third = third.split(",")
+        x3 = float(third[0])
+        y3 = float(third [1])
 
-area = triangle_area(x1, y1, x2, y2, x3, y3)
-print(f"The area of the triangle with vertices ({first[0]},{first[1]}), ({second[0]},{second[1]}), and ({third[0]},{third[1]}) is {area} square km.")
+        area = triangle_area(x1, y1, x2, y2, x3, y3)    #calls on triangle area function using the recorded inputs
+        print(f"The area of the triangle with vertices ({first[0]},{first[1]}), ({second[0]},{second[1]}), and ({third[0]},{third[1]}) is {area} square km.")
+    except:     #if there's an error, skips to this step and restarts the while True loop
+        print("These points are not a valid triangle. Please try again, and make sure you are using the x,y syntax!")

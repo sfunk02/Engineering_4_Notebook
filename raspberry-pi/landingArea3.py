59dbd1ab752ccff2c88745d58d5da52bcb782701
sfunk02 <sfunk02@charlottesvilleschools.org>
2022-10-14 10:35:37 -0400

    updated landingArea3

--- raspberry-pi/landingArea3.py
+++ raspberry-pi/landingArea3.py
@@ -11,13 +11,13 @@ import displayio
 import time
 import math
 
-points = [[-50,-17,-57,12,-22,-7],[28,-14,60,-7,54,18],[45,30,51,-1,18,6],[5,5,19,15,22,10]]
+points = [[-2,-30,-19,-8,-44,-18],[7,-14,60,-7,33,-6],[5,5,-8,9,0,-6],[63,30,60,19,29,16]]  #[x1,y1,x2,y2,x3,y3]
 
 def triangle_area(x1, y1, x2, y2, x3, y3):  #defines a function for triangle area that takes six inputs
     total = (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2 #the math behind the function, using the six inputs
     return abs(total)   #returns absolute value of previous math
 
-def centroid_distance(x1, y1, x2, y2, x3, y3):
+def centroid_distance(x1, y1, x2, y2, x3, y3):  #function to determine centroid distance from the origin
     centerx = ((x1 + x2 + x3) / 3)
     centery = ((y1 + y2 + y3) / 3)
     return math.sqrt((centerx * centerx) + (centery * centery))
@@ -32,10 +32,11 @@ display_bus = displayio.I2CDisplay(i2c, device_address=0x3d, reset=board.GP0)   
 display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64)
 
 i = 0
+best = points[0]
 
 while True:
-    if i == 4:
-        break
+    if i == 4:  #(after all points have been used)
+        break   #ends loop
     else:
         area = triangle_area(points[i][0], points[i][1], points[i][2], points[i][3], points[i][4], points[i][5])
         distance = centroid_distance(points[i][0], points[i][1], points[i][2], points[i][3], points[i][4], points[i][5])
@@ -54,6 +55,8 @@ while True:
         splash.append(vline)
         splash.append(text_area)
         display.show(splash)
-        time.sleep(3)
-        i += 1
-print(f"The closest suitable landing area has vertices ({points[i][0]},{points[i][1]}), ({points[i][2]},{points[i][3]}), and ({points[i][4]},{points[i][5]}) . The area is {area} km^2 and the centroid is {distance} km away from base.")
+        if area > 100 and distance < centroid_distance(best[0],best[1],best[2],best[3],best[4],best[5]):    
+            best = points[i]    #sets current triangle to "best" if it meets the requirements
+        time.sleep(1)
+        i += 1  #cycles to the next triangle
+print(f"The closest suitable landing area has vertices ({best[0]},{best[1]}), ({best[2]},{best[3]}), and ({best[4]},{best[5]}). The area is {triangle_area(best[0],best[1],best[2],best[3],best[4],best[5])} km^2 and the centroid is {centroid_distance(best[0],best[1],best[2],best[3],best[4],best[5])} km away from base.")
